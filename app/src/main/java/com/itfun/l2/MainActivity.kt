ffcc2476110e2abbdbf49b62e028d2feed92afce
JluckyStrike <it.fun@mail.ru>
2022-01-12 20:25:44 +0500

    Приложение для поиска фильмов. Примерная разметка, логика.

--- app/src/main/java/com/itfun/l2/MainActivity.kt
+++ app/src/main/java/com/itfun/l2/MainActivity.kt
@@ -2,17 +2,40 @@ package com.itfun.l2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.itfun.l2.fragment_nav.FavouriteFragment
 import com.itfun.l2.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val favouriteFragment = FavouriteFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+
+        findViewById<BottomNavigationView>(R.id.nav_view).setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> replaceFragment(MainFragment.newInstance())
+                R.id.navigation_dashboard -> {
+                }
+                R.id.navigation_favourite -> replaceFragment(favouriteFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        if (fragment != null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.container, fragment)
+            transaction.commit()
         }
     }
 }
---
+++ app/src/main/java/com/itfun/l2/Movie.kt
@@ -0,0 +1,4 @@
+package com.itfun.l2
+
+
+data class Movie(val imageMovie: Int, val titleMovie: String, val ratingMovie: Double)
